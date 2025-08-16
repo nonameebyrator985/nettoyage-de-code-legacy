31c123a5c45dca7b43f5d051e45b12c79e358ccd
nonameebyrator985 <singheingemo@hotmail.com>
2025-08-17 00:22:23 +0900

    Fix README file reading to close the file properly

--- setup.py
+++ setup.py
@@ -17,3 +17,25 @@ setup(
     ],
     python_requires='>=3.6',
 )
+
+# Ensure that the README file is closed after reading to free up resources
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nettoyage-de-code-legacy',
+    version='0.1',
+    packages=find_packages(),
+    description='Un outil Python pour analyser et nettoyer le code hérité.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/votre-utilisateur/nettoyage-de-code-legacy',
+    author='Votre Nom',
+    author_email='votre.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
