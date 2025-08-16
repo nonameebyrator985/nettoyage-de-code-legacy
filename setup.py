61b5cb1e22e7dfee73bde6eb7357c060bfa3e220
nonameebyrator985 <singheingemo@hotmail.com>
2025-08-17 00:14:26 +0900

    feat: initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nettoyage-de-code-legacy',
+    version='0.1',
+    packages=find_packages(),
+    description='Un outil Python pour analyser et nettoyer le code hérité.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/votre-utilisateur/nettoyage-de-code-legacy',
+    author='Votre Nom',
+    author_email='votre.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
